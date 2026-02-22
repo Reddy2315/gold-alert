6d2f6e89747337aed491dfe39c255891bf4c96eb
Nagendharreddy Kondapu <nagendharreddy22@gmail.com>
2026-02-22 16:36:45 +0530

    👌 IMPROVE: Added set from mail for brevo

--- gold-alert/src/main/java/com/goldalert/service/EmailNotificationService.java
+++ gold-alert/src/main/java/com/goldalert/service/EmailNotificationService.java
@@ -4,6 +4,7 @@ import com.goldalert.model.Alert;
 import com.goldalert.template.NotificationTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -18,11 +19,18 @@ public class EmailNotificationService implements NotificationService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.from}")
+    private String mailFrom;
+
     @Async
     @Override
     public void notify(Alert alert, double currentPrice) {
         log.info("📧 EMAIL TRIGGERED for: {}" , alert.getUser().getEmail());
+
         SimpleMailMessage message = new SimpleMailMessage();
+
+        // REQUIRED FOR BREVO
+        message.setFrom(mailFrom);
         message.setTo(alert.getUser().getEmail());
         message.setSubject("Gold Buy Alert 🚨");
         message.setText(
